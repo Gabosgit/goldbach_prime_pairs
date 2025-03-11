eb3f9ac45ab547ae2560a75ba1fd9b9de1f603fd
Gabo <no@email>
2025-03-11 16:09:30 +0100

    Submited a new version Goldbach_v2. UPDATED Docstrings. A function is_prime() what separeted from the function get_prime_pairs()

--- Goldbach_v2.py
+++ Goldbach_v2.py
@@ -1,6 +1,13 @@
-""" This script gets pairs of prime numbers that when added together result in an even number """
+"""
+    This script gets an even number and returns pairs of numbers that when added together equal this even number.
+    A number is only calculated once in the list of resulting pairs.
+"""
 
 def is_prime(num):
+    """
+    param num: get a number 'num'
+    :return: True if the number is prime and False if the number is not prime
+    """
     num_is_prime = True
     divisor = 2
     while divisor < num:
@@ -10,34 +17,25 @@ def is_prime(num):
     return num_is_prime
 
 def get_prime_pairs(number):
-    """gets pairs of prime numbers that when added together result in an even number"""
+    """
+    Gets a number. It is even calculate the prime pairs that summed are equal to this even number.
+    Checks prime numbers with the function is_prime().
+    A prime number is not repeated in the list of pairs.
+    :param number: entered by user
+    :return: list of prime pars
+    """
     list_num1_primes = []
     list_prime_num1_num2_pairs = []
     if number % 2 == 1: # Break if number is odd
         return False
-
     for num1 in range(2, number): # check if num1 is a prime
-        # num1_is_prime = True
-        # divisor = 2
-        # while divisor < num1:
-        #     if num1 % divisor == 0:
-        #         num1_is_prime = False
-        #     divisor += 1
         num1_is_prime = is_prime(num1)
-
         if num1_is_prime:
             list_num1_primes.append(num1)
             # check if num2 = number - num1 is prime
             num2 = number - num1
             if num2 >= 2:
-                # num2_is_prime = True
-                # divisor = 2
-                # while divisor < num2:
-                #     if num2 % divisor == 0:
-                #         num2_is_prime = False
-                #     divisor += 1
                 num2_is_prime = is_prime(num2)
-
                 # Prints if num1 and num2 are prime and don't repeat pairs numbers
                 if num2_is_prime and num2 not in list_num1_primes: # if num2 was not printed as num1
                     list_prime_num1_num2_pairs.append((num1, num2))
@@ -71,6 +69,7 @@ def main():
             print(f"ERROR: You did NOT enter a number.\n{error}")
         except TypeError:
             print(f"ERROR: You did NOT enter an even number.")
+
 
 if __name__ == '__main__':
     main()
